23945d956e133ceb5f1f006c91b6d19ffc9073fc
somdari <matthewchoi7@gmail.com>
2017-12-08 13:25:03 +1100

    Filter component

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import './App.css';
+import Filter from './components/Filter/Filter';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <Filter />
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ src/components/Filter/Filter.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import './Filter.css';
+
+const Filter = () => (
+  <nav className="filter-container">
+    <div className="filter">
+      <h1>Women's tops</h1>
+      <select className="filter__field" name="" id="">
+        <option value="/">Filter by size</option>
+      </select>
+    </div>
+  </nav>
+)
+
+export default Filter;
+
+
