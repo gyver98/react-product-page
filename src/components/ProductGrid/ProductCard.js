74a87ac8096bc0e7cabed334d7c021fdd049c495
somdari <matthewchoi7@gmail.com>
2017-12-09 13:03:34 +1100

    update product-badge images

--- src/components/ProductGrid/ProductCard.js
+++ src/components/ProductGrid/ProductCard.js
@@ -5,6 +5,7 @@ const ProductCard = (props) => (
     <div className="product-image">
       <img src={require(`../../assets/${props.product.productImage}`)} alt="" />
     </div>
+    <div className={`product-badge ${props.product.isSale ? 'product-badge--sale': (props.product.isExclusive ? 'product-badge--exclusive': '')}`}></div>
     <div className="product-info">
       <h5 className="product-info__item">{props.product.productName}</h5>
       <h6 className="product-info__price">{props.product.price}</h6>
